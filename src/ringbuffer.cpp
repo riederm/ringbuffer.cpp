7366896705bb8c700348aad1f055b145cdd73904
Mathias Rieder <mathias.rieder@gmail.com>
2022-10-17 12:35:53 +0000

    initial solution

--- src/ringbuffer.cpp
+++ src/ringbuffer.cpp
@@ -9,33 +9,22 @@ RingBuffer::RingBuffer(int _maxLength) {
   len = 0;
 }
 
-void RingBuffer::move_rd() {
-  r = (r + 1) % maxLength;
-  if (w == r) {
-    // we just moved onto the read-ptr
-    move_wr();
-  }
-}
-
-void RingBuffer::move_wr() {
-  w = (w + 1) % maxLength;
-  if (w == r) {
-    // we just moved onto the read-ptr
-    move_rd();
-  }
-}
-
 void RingBuffer::add(int value) {
   data[w] = value;
+ 
+  w = (w + 1) % maxLength;
+  if (len == maxLength) {
+    r = (r + 1) % maxLength;
+  }
+
   len = std::min(maxLength, len + 1);
-  move_wr();
 }
 
 bool RingBuffer::remove(int *result) {
   if (length() > 0) {
     *result = data[r];
     len--;
-    move_rd();
+    r = (r + 1) % maxLength;
     return true;
   }
   return false;
--- test/ringbuffer.test.cpp
+++ test/ringbuffer.test.cpp
@@ -45,3 +45,51 @@ TEST(ringbuffer, reading_returns_items_in_order) {
     ASSERT_EQ(result, i);
   }
 }
+
+TEST(ringbuffer, reading_after_wrap_around) {
+  // GIVEN a full ringbuffer 0-4
+  auto buffer = RingBuffer(5);
+  for (int i = 0; i < 5; i++) {
+    buffer.add(i);
+  }
+
+  // WHEN I add two additional items 5,6
+  buffer.add(5);
+  buffer.add(6);
+
+  // THEN I expect 2-6
+  for (int i = 2; i < 7; i++) {
+    int result;
+    ASSERT_TRUE(buffer.remove(&result));
+    ASSERT_EQ(result, i);
+  }
+}
+
+TEST(ringbuffer, reading_empty_buffer_returns_false) {
+  // GIVEN an empty buffer
+  auto buffer = RingBuffer(5);
+
+  // WHEN I read from it
+  int result;
+  bool success = buffer.remove(&result);
+
+  // THEN I expect false
+  ASSERT_FALSE(success);
+}
+
+TEST(ringbuffer, adding_a_lot_of_entries) {
+  // GIVEN an empty buffer
+  auto buffer = RingBuffer(5);
+
+  // WHEN I add a lot of elements 0-99
+  for(int i=0; i<100; i++){
+    buffer.add(i);
+  }
+
+  // THEN I the elements 95, 96, 97, 98, 99
+  for (int i = 95; i < 100; i++) {
+    int result;
+    ASSERT_TRUE(buffer.remove(&result));
+    ASSERT_EQ(result, i);
+  }
+}
